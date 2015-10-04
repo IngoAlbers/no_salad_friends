46cb9cdf1c4f0bf40f89f02f546029e696b88f45
Ingo Albers <pjotrpansen@gmail.com>
2015-10-04 14:23:36 +0200

    Add jQuery and simple audio player

--- www/js/index.js
+++ www/js/index.js
@@ -16,36 +16,15 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function onDeviceReady() {
 
-        console.log('Received Event: ' + id);
-    }
-};
+  $('#play-button').click(function () {
+    $('#audio-player').trigger('play');
+  });
 
-app.initialize();
+  $('#stop-button').click(function () {
+    $('#audio-player').trigger('pause');
+  });
+}
