819d2871ceeda0ac8586ec8b732bd9d6382b2030
IngoAlbers <pjotrpansen@gmail.com>
2016-04-15 10:41:57 +0200

    fix formatting

--- hooks/after_prepare/add_to_manifest.js
+++ hooks/after_prepare/add_to_manifest.js
@@ -3,16 +3,16 @@
 // Adds lines to the Android Manifest if they are not already there
 
 const LINES_TO_ADD = [
-    {
-        text: ' android:installLocation="auto"',
-        after: '<manifest'
-    }
+  {
+    text: ' android:installLocation="auto"',
+    after: '<manifest'
+  }
 ];
 
 const MANIFEST = 'platforms/android/AndroidManifest.xml';
 
 var fs = require('fs'),
-  manifestText = fs.readFileSync(MANIFEST).toString();
+    manifestText = fs.readFileSync(MANIFEST).toString();
 
 LINES_TO_ADD.forEach(function(lineToAdd) {
   if(manifestText.indexOf(lineToAdd.text) === -1) {
