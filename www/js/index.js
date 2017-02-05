80d69401af0023b4559540a50f91a934d31a5a04
Michael Blum <blummichael@gmail.com>
2017-02-05 17:24:31 +0100

    Fixes #34 - phyical back button should behave the same as app-back-button

--- www/js/index.js
+++ www/js/index.js
@@ -113,6 +113,7 @@ function onBackKeyDown() {
   // console.log("onBackKeyDown()");
   // If we are on a subpage, go back to main-page
   if(!$(".main").hasClass("page-active")){
+    stopSound(false);
     changePage("main");
   }
   // If we are on the main-page, close the app
